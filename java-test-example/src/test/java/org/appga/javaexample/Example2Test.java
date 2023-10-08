f4f208b9babc1fbf1cca01046eaa2091c516379a
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2023-10-08 22:54:58 +0200

    Add simple java example project

---
+++ java-test-example/src/test/java/org/appga/javaexample/Example2Test.java
@@ -0,0 +1,20 @@
+package org.appga.javaexample;
+
+import org.appga.junit5watcher.BenchmarkExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BenchmarkExtension.class)
+public class Example2Test {
+
+    @Test
+    void test1() throws InterruptedException {
+        Thread.sleep(1_000);
+    }
+
+    @Test
+    void test2() throws InterruptedException {
+        Thread.sleep(1_000);
+    }
+
+}
