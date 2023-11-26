e2dc4f1657761dc7e303baf1ef83a859d26fd043
Grzegorz Aniol <grzegorz.aniol@graphaware.com>
2023-11-26 14:42:17 +0100

    Bump dependency versions

--- spring-boot-test-example/src/main/kotlin/pl/appga/junit5watcher/example/entities/Uzer.kt
+++ spring-boot-test-example/src/main/kotlin/pl/appga/junit5watcher/example/entities/Uzer.kt
@@ -1,8 +1,8 @@
 package pl.appga.junit5watcher.example.entities
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 
 @Entity
 class Uzer (
